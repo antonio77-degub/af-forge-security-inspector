e8ce7ea9cf58d1e6abde949747c307394f8e6769
antonio77-degub <antoniocff77@gmail.com>
2026-09-22 01:43:52 +0100

    feat(auth): route password recovery completion

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { AppShell } from '../components/layout/AppShell';
 import { AuthCallbackPage } from '../features/auth/AuthCallbackPage';
 import { ProtectedRoute } from '../features/auth/ProtectedRoute';
+import { ResetPasswordPage } from '../features/auth/ResetPasswordPage';
 import { SignInPage } from '../features/auth/SignInPage';
 import { HomePage } from '../pages/HomePage';
 import { NotFoundPage } from '../pages/NotFoundPage';
@@ -24,6 +25,7 @@ export function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<SignInPage />} />
         <Route path="/auth/callback" element={<AuthCallbackPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
         {publicModules.map(([path, title]) => <Route key={path} path={`/${path}`} element={<PlaceholderPage title={title} />} />)}
         {protectedModules.map(([path, title]) => (
           <Route key={path} path={`/${path}`} element={<ProtectedRoute><PlaceholderPage title={title} /></ProtectedRoute>} />
